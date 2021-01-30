ea9b1502592114058365b838d86cbc6364378468
Uzay Kaya <uzay.kaya@cloudnesil.com>
2021-01-30 20:18:16 +0300

    Test is added for "Birthday Cake Candles" problem

---
+++ HackerRank_Solutions/Birthday-Cake-Candles/birtday-cake-candles_test.go
@@ -0,0 +1,17 @@
+package Birthday_Cake_Candles
+
+import "testing"
+
+func TestBirthDayCakeCandles(t *testing.T) {
+
+	t.Run("collection of any size", func(t *testing.T) {
+		candles := []int32{4, 4, 1, 2}
+
+		got := birthdayCakeCandles(candles)
+		expected := int32(2)
+
+		if got != expected {
+			t.Errorf("got %d expected %d of given %v", got, expected, candles)
+		}
+	})
+}
---
+++ HackerRank_Solutions/Birthday-Cake-Candles/birthday-cake-candles.go
@@ -0,0 +1,6 @@
+package Birthday_Cake_Candles
+
+func birthdayCakeCandles(candles []int32) int32 {
+	// Write your code here
+	return 0
+}
