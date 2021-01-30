e966fa84b3515f6d852d84293915005f95c152f6
Uzay Kaya <uzay.kaya@cloudnesil.com>
2021-01-30 20:00:10 +0300

    Update variable type because 32 bit was not enough for big integers

--- HackerRank_Solutions/Mini-Max-Sum/mini-max-sum.go
+++ HackerRank_Solutions/Mini-Max-Sum/mini-max-sum.go
@@ -3,17 +3,17 @@ package Mini_Max_Sum
 func MiniMaxSum(arr [5]int32) (int, int) {
 	minValue := arr[0]
 	maxValue := arr[0]
-	var totalSum int32 = 0
+	var totalSum int64 = 0
 	for _, v := range arr {
 		if v < minValue {
 			minValue = v
-		} else {
+		} else if v > maxValue {
 			maxValue = v
 		}
-		totalSum += v
+		totalSum += int64(v)
 	}
-	maxSum := totalSum - minValue
-	minSum := totalSum - maxValue
+	maxSum := totalSum - int64(minValue)
+	minSum := totalSum - int64(maxValue)
 
 	return int(minSum), int(maxSum)
 }
--- HackerRank_Solutions/Mini-Max-Sum/mini-max-sum_test.go
+++ HackerRank_Solutions/Mini-Max-Sum/mini-max-sum_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestSum(t *testing.T) {
 
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int32{2, 1, 3, 4, 5}
+		numbers := [5]int32{156873294, 719583602, 581240736, 605827319, 895647130}
 
 		minSum, maxSum := MiniMaxSum(numbers)
-		expectedMinSum := 10
-		expectedMaxSum := 14
+		expectedMinSum := 2063524951
+		expectedMaxSum := 2802298787
 
 		if minSum != expectedMinSum || maxSum != expectedMaxSum {
 			t.Errorf("minSum %d maxSum %d expectedMinSum %d expectedMaxSum %d given, %v", minSum, maxSum, expectedMinSum, expectedMaxSum, numbers)
