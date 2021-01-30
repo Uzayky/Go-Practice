b6a13ef7bc77b07ccdce93fe0ce9ad299d8fdb4b
Uzay Kaya <uzay.kaya@cloudnesil.com>
2021-01-30 22:25:37 +0300

    Test is added for "Number Line Jumps" problem

---
+++ HackerRank_Solutions/Number-Line-Jumps/number-line-jumps.go
@@ -0,0 +1,8 @@
+package Number_Line_Jumps
+
+// https://www.hackerrank.com/challenges/kangaroo/problem
+
+func Kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
+
+	return ""
+}
---
+++ HackerRank_Solutions/Number-Line-Jumps/number-line-jumps_test.go
@@ -0,0 +1,31 @@
+package Number_Line_Jumps
+
+import (
+	"testing"
+)
+
+func TestKangaroo(t *testing.T) {
+
+	isKangaroosOnSameSpot := func(t testing.TB, got, expected string) {
+		t.Helper()
+
+		if got != expected {
+			t.Errorf("got %s expected %s", got, expected)
+		}
+	}
+
+	t.Run("", func(t *testing.T) {
+
+		got := Kangaroo(0, 3, 4, 2)
+		expected := "YES"
+		isKangaroosOnSameSpot(t, got, expected)
+	})
+
+	t.Run("", func(t *testing.T) {
+
+		got := Kangaroo(0, 2, 5, 3)
+		expected := "NO"
+		isKangaroosOnSameSpot(t, got, expected)
+
+	})
+}
