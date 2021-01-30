5b50df8b481b2026ba370e4618b1af27b7616b0c
Uzay Kaya <uzay.kaya@cloudnesil.com>
2021-01-30 18:58:35 +0300

    Function is updated with minimum requirement HackerRank "Mini-Max-Sum" problem

--- HackerRank_Solutions/Mini-Max-Sum/mini-max-sum.go
+++ HackerRank_Solutions/Mini-Max-Sum/mini-max-sum.go
@@ -1,5 +1,19 @@
 package Mini_Max_Sum
 
 func MiniMaxSum(arr [5]int32) (int, int) {
-	return 0, 0
+	minValue := arr[0]
+	maxValue := arr[0]
+	var totalSum int32 = 0
+	for _, v := range arr {
+		if v < minValue {
+			minValue = v
+		} else {
+			maxValue = v
+		}
+		totalSum += v
+	}
+	maxSum := totalSum - minValue
+	minSum := totalSum - maxValue
+
+	return int(minSum), int(maxSum)
 }
--- HackerRank_Solutions/Mini-Max-Sum/mini-max-sum_test.go
+++ HackerRank_Solutions/Mini-Max-Sum/mini-max-sum_test.go
@@ -4,13 +4,15 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int32{2, 1, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int32{2, 1, 3, 4, 5}
 
-	minSum, maxSum := MiniMaxSum(numbers)
-	expectedMinSum := 14
-	expectedMaxSum := 10
+		minSum, maxSum := MiniMaxSum(numbers)
+		expectedMinSum := 10
+		expectedMaxSum := 14
 
-	if minSum != expectedMinSum || maxSum != expectedMaxSum {
-		t.Errorf("minSum %d maxSum %d expectedMinSum %d expectedMaxSum %d given, %v", minSum, maxSum, expectedMinSum, expectedMaxSum, numbers)
-	}
+		if minSum != expectedMinSum || maxSum != expectedMaxSum {
+			t.Errorf("minSum %d maxSum %d expectedMinSum %d expectedMaxSum %d given, %v", minSum, maxSum, expectedMinSum, expectedMaxSum, numbers)
+		}
+	})
 }
