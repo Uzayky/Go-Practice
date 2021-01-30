acc980969479b86f2bae95cd18e9ff04eff531a9
Uzay Kaya <uzay.kaya@cloudnesil.com>
2021-01-30 20:09:02 +0300

    Description is added for Mini-Max Sum problem

--- HackerRank_Solutions/Mini-Max-Sum/mini-max-sum.go
+++ HackerRank_Solutions/Mini-Max-Sum/mini-max-sum.go
@@ -1,6 +1,9 @@
 package Mini_Max_Sum
 
+// https://www.hackerrank.com/challenges/mini-max-sum/problem Solution
+
 func MiniMaxSum(arr [5]int32) (int, int) {
+	// Calculate minimum, maximum value and subtracting the calculated values from the total, we arrive at the conclusion
 	minValue := arr[0]
 	maxValue := arr[0]
 	var totalSum int64 = 0
