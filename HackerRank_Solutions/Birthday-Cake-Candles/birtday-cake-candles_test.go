cc2b8cfbfca3320e2ba73e4cb5dc7f69e6773073
Uzay Kaya <uzay.kaya@cloudnesil.com>
2021-01-30 20:29:30 +0300

    Function is added for "Birthday Cake Candles" problem

--- HackerRank_Solutions/Birthday-Cake-Candles/birtday-cake-candles_test.go
+++ HackerRank_Solutions/Birthday-Cake-Candles/birtday-cake-candles_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestBirthDayCakeCandles(t *testing.T) {
 
 	t.Run("collection of any size", func(t *testing.T) {
-		candles := []int32{4, 4, 1, 2}
+		candles := []int32{4, 4, 1, 2, 6, 5, 6}
 
 		got := birthdayCakeCandles(candles)
 		expected := int32(2)
--- HackerRank_Solutions/Birthday-Cake-Candles/birthday-cake-candles.go
+++ HackerRank_Solutions/Birthday-Cake-Candles/birthday-cake-candles.go
@@ -1,6 +1,19 @@
 package Birthday_Cake_Candles
 
+// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
 func birthdayCakeCandles(candles []int32) int32 {
-	// Write your code here
-	return 0
+	maxValue := candles[0]
+	var candleCount int32 = 0
+	for _, v := range candles {
+		if v > maxValue {
+			maxValue = v
+			candleCount = 0
+		}
+		if v == maxValue {
+			candleCount++
+		}
+	}
+
+	return candleCount
 }
